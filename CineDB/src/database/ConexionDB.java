b35f11ba4f24a36e6edb16b30b335df64d15ba81
Kevi-sito <deleon.martinez.24079@itsmante.edu.mx>
2026-05-06 23:34:35 -0600

    feat: clase UserDAO

--- CineDB/src/database/ConexionDB.java
+++ CineDB/src/database/ConexionDB.java
@@ -1,7 +1,9 @@
 package database;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+
 public class ConexionDB {
     
     private static Connection connection;
